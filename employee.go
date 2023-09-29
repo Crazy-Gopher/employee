960db2330f22948c0c6e040a042b920a5b377405
Kapil Jain <kapiljain.jain93@gmail.com>
2023-09-29 21:06:00 +0530

    Updated

--- employee.go
+++ employee.go
@@ -1,7 +1,15 @@
 package employee
 
-import "github.com/Crazy-Gopher/department"
+import (
+	"strings"
+
+	"github.com/Crazy-Gopher/department"
+)
 
 func Greet(msg string) string {
 	return "Hello" + " " + department.MagicString(msg)
+}
+
+func GreetCapital(msg string) string {
+	return strings.ToUpper("Hello" + " " + department.MagicString(msg))
 }
